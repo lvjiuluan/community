51eb2868ea6d637c7f45f4a59b79b7a86b533297
lvjiuluan_global_level <lvjiuluan_global_level@163.com>
2024-06-19 22:41:49 +0800

    创建springboot项目

---
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommunityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
