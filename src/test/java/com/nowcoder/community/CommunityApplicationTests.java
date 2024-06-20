49c5a7cd57f2e518460d4a4de2dc629493ba3b75
lvjiuluan_global_level <lvjiuluan_global_level@163.com>
2024-06-20 22:14:41 +0800

    zhangsan 实现接口并测试

--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -3,13 +3,29 @@ package com.nowcoder.community;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 public class CommunityApplicationTests {
 
     @Test
     public void contextLoads() {
+        assertEquals(8, quickPowIterative(2, 3));
+        assertEquals(1, quickPowIterative(2, 0));
+        assertEquals(0, quickPowIterative(0, 5));
+        assertEquals(-8, quickPowIterative(-2, 3));
     }
 
     // 实现一个快速幂计算的方法
-
+    public long quickPowIterative(long base, long exponent) {
+        long result = 1;
+        while (exponent > 0) {
+            if ((exponent & 1) == 1) {
+                result *= base;
+            }
+            base *= base;
+            exponent >>= 1;
+        }
+        return result;
+    }
 }
